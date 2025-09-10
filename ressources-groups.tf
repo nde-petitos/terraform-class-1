7ee2670725def5de2b6f0d3144ac11b430453035
nde-petitos <nde.petitos@gmail.com>
2025-09-10 18:53:00 -0400

    Create ressources-groups.tf
    Commit creating the resources group file

---
+++ ressources-groups.tf
@@ -0,0 +1,8 @@
+resource "azurerm_resource_group" "terraformmay022025" {
+  name     = "mcit_resource_group_may022025"
+  location = "canadacentral"
+}
+resource "azurerm_resource_group" "terraformseptember" {
+  name     = "mcit_resource_group_sep092025"
+  location = "canadacentral"
+}
