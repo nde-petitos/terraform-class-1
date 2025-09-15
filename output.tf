9255a62ddc5a9f08b6f76f9ea8280356cbae6b1c
nde-petitos <nde.petitos@gmail.com>
2025-09-15 19:17:57 -0400

    split code

--- output.tf
+++ output.tf
@@ -1,30 +1,34 @@
-output "first-name" {
-  value = var.firstname
-}
+# output "first-name" {
+#   value = var.firstname
+# }
 
-output "last-name" {
-  value = var.lastname
-}
+# output "last-name" {
+#   value = var.lastname
+# }
 
-output "year-of-arrival" {
-  value = var.arrivalyear
-}
+# output "year-of-arrival" {
+#   value = var.arrivalyear
+# }
 
-output "list-of-restaurant" {
- value = tolist(var.listofrestaurants)
-}
+# output "list-of-restaurant" {
+#  value = tolist(var.listofrestaurants)
+# }
 
-output "random-number" {
-  value = var.randomnumber
-}
+# output "random-number" {
+#   value = var.randomnumber
+# }
 
-output "display-a-boolean" {
-  value = var.isahuman
-}
+# output "display-a-boolean" {
+#   value = var.isahuman
+# }
 
-output "display-favorite-five" {
-  value = {for key, listvalues in var.favoritefive : key => listvalues}
+# output "display-favorite-five" {
+#   value = {for key, listvalues in var.favoritefive : key => listvalues}
+# }
+#  output "application_name" {
+#   value = local.full_name
+#  }
+
+output "display_split_server_name" {
+  value = split("-", var.server_name)
 }
- output "application_name" {
-  value = local.full_name
- }
--- variables.tf
+++ variables.tf
@@ -1,65 +1,70 @@
-variable "subscription_id" {
-  type = string
-}
+# variable "subscription_id" {
+#   type = string
+# }
     
-variable "client_id" {
-  type = string
-}
+# variable "client_id" {
+#   type = string
+# }
 
-variable "client_secret" {
-  type = string
-}
+# variable "client_secret" {
+#   type = string
+# }
 
-variable "tenant_id" {
-  type = string
-}
+# variable "tenant_id" {
+#   type = string
+# }
 
-variable "arrivalyear" {
-  type = number
-  default = 2025
-}
+# variable "arrivalyear" {
+#   type = number
+#   default = 2025
+# }
 
-variable "firstname" {
-  type = string
-  default = "Nelson"
-}
+# variable "firstname" {
+#   type = string
+#   default = "Nelson"
+# }
 
-variable "lastname" {
-  type = string
-  default = "Petitos"
-}
+# variable "lastname" {
+#   type = string
+#   default = "Petitos"
+# }
 
-variable "listofrestaurants" {
-  type    = set(string)
-  default = ["Restaurant 1 ", "Restaurant 2 ", "Restaurant 3"]
-}
+# variable "listofrestaurants" {
+#   type    = set(string)
+#   default = ["Restaurant 1 ", "Restaurant 2 ", "Restaurant 3"]
+# }
 
-variable "favoritefive" {
-  type = map(list(string))
-  default = {
-    fruits = ["Avocado", "Mango", "Pineaple", "Apple", "Orange", "Grappe"]
-    movies = ["Jango", "Hoow to train your dragon", "Shuttle islan", "The Prestige", "Deadpool", "Lord of the rings"]
-    travel = ["Paris", "London", "Cairo", "Yaounde", "Tokyo"]
-    workout = ["Legs", "Arms", "Torso", "Pecs", "Abs", "Full body"]
-    foods = ["Eru", "Spaghetti", "Ndoleh", "Taro", "Burger", "Tacos"]
-    colors = ["Red", "Blue", "Green", "White", "Yellow", "Black"]
-    songs = ["Noir desirs", "A lombre du chaud business", "Luther", "How much a dollar cost", "Roar", "Thriller"]
-    emojis = ["😂", "😎", "😝", "😭", "🤬", "😒"]
-    affirmations = ["Life is short", "What doesnt kill you make you stronger", "I have a dream", "Life is a matter of perpective", "GOD is good", "JESUS save"]
-  }
-}
+# variable "favoritefive" {
+#   type = map(list(string))
+#   default = {
+#     fruits = ["Avocado", "Mango", "Pineaple", "Apple", "Orange", "Grappe"]
+#     movies = ["Jango", "Hoow to train your dragon", "Shuttle islan", "The Prestige", "Deadpool", "Lord of the rings"]
+#     travel = ["Paris", "London", "Cairo", "Yaounde", "Tokyo"]
+#     workout = ["Legs", "Arms", "Torso", "Pecs", "Abs", "Full body"]
+#     foods = ["Eru", "Spaghetti", "Ndoleh", "Taro", "Burger", "Tacos"]
+#     colors = ["Red", "Blue", "Green", "White", "Yellow", "Black"]
+#     songs = ["Noir desirs", "A lombre du chaud business", "Luther", "How much a dollar cost", "Roar", "Thriller"]
+#     emojis = ["😂", "😎", "😝", "😭", "🤬", "😒"]
+#     affirmations = ["Life is short", "What doesnt kill you make you stronger", "I have a dream", "Life is a matter of perpective", "GOD is good", "JESUS save"]
+#   }
+# }
 
-variable "randomnumber" {
-  type = number
-  default = 200345
-}
+# variable "randomnumber" {
+#   type = number
+#   default = 200345
+# }
 
-variable "isahuman" {
-  type = bool
-  default = false
-}
+# variable "isahuman" {
+#   type = bool
+#   default = false
+# }
 
-variable "app_name" {
-  type = string
-  default = "terraform-test"
+# variable "app_name" {
+#   type = string
+#   default = "terraform-test"
+# }
+
+variable "server_name" {
+  type = string 
+  default = "server-prod-01"
 }
