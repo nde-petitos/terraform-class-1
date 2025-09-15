c22cecf02ba38ceb5290023961ab3c595adea8fb
nde-petitos <nde.petitos@gmail.com>
2025-09-15 11:40:30 -0400

    Update variables.tf
    Add top five favorites of many categories

--- variables.tf
+++ variables.tf
@@ -34,6 +34,21 @@ variable "listofrestaurants" {
   default = ["Restaurant 1 ", "Restaurant 2 ", "Restaurant 3"]
 }
 
+variable "favoritefive" {
+  type = map(list(string))
+  default = {
+    Fruits = ["Avocado", "Mango", "Pineaple", "Apple", "Orange", "Grappe"];
+    Movies = ["Jango", "Hoow to train your dragon", "Shuttle islaan", "The Prestige, "Deadpool", "Lord of the rings"];
+    Travel = ["Paris: , "London", "Cairo", "Yaounde", "Tokyo"];
+    Workout = ["Legs", "Arms", "Torso", "Pecs", "Abs", ""];
+    Foods = ["Eru", "Spaghetti", "Ndoleh", "Taro", "Burger", "Tacos"];
+    Colors = ["Red", "Blue", "Green", "White", "Yellow", "Black"];
+    Songs = ["Noir desirs", "a l'ombre du chaud business", "Luther", "How much a dollar cost", "Roar", "Thriller"];
+    Emojis = ["😂", "😎", "😝", "😭", "🤬", "😒"];
+    Affirmations = ["Life is short", "What doesnt kill you make you stronger", "I have a dream", "Life is a matter of perpective", "GOD is good", "JESUS save"];
+  }
+}
+
 variable "randomnumber" {
   type = number
   default = 200345
