bdf0da2f3d4b74eb91637c9693c69e7c62289044
nde-petitos <nde.petitos@gmail.com>
2025-09-15 20:25:46 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,12 +1,16 @@
-locals {
+#locals {
     # full_name = "${var.app_name}-prod"
-}
+#}
+
+#locals {
+  #movie_lower = lower(var.movie)
+ # movie_upper = upper(var.movie)
+#}
+
+#locals {
+ # replaced = replace(var.original, "MCIT", "Montreal College")
+#}
 
 locals {
-  movie_lower = lower(var.movie)
-  movie_upper = upper(var.movie)
-}
-
-locals {
-  replaced = replace(var.original, "MCIT", "Montreal College")
+  first_word = substr(var.phrase, 0, 9)
 }
