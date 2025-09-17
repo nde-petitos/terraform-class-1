4e785897c6e19aa4b36bd988d458784ad3d0477f
nde-petitos <nde.petitos@gmail.com>
2025-09-17 18:50:23 -0400

    Create Exercise2.tf
    Exercise 2

---
+++ Exercise2.tf
@@ -0,0 +1,14 @@
+variable "usernames" {
+ type    = list(string) # Define the variable usernames as a list of string
+ default = ["alice", "bob", "carol"] # Give default array of string to that variable 
+}
+variable "domain" {
+ type    = string # Define the type string as the type of the variable domain
+ default = "example.com" # Give it the value "example.com as default value"
+}
+locals {
+ emails = { for u in var.usernames : u => "${u}@${var.domain}" } # Add example.com as surfix to every string in the array usernames
+}
+output "emails" {
+ value = local.emails # Display the result
+}
