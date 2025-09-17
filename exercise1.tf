ce3f526f42d1b33d2f88a171c6b7b990b7f64f2b
nde-petitos <nde.petitos@gmail.com>
2025-09-17 18:37:29 -0400

    Create exercise1.tf
    Exercise 1 file create

---
+++ exercise1.tf
@@ -0,0 +1,12 @@
+variable "labels" {
+ type    = set(string) # Define the variable as a list of dtring
+ default = ["alpha", "beta", "gamma"] # List of default value
+}
+
+locals {
+ labels_upper = toset([for s in var.labels : upper(s)]) # Make every string in the set as upper case and then transform it to a set again
+}
+
+output "labels_upper" {
+ value = local.labels_upper # Display the labels_upper
+}
