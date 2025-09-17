44c7f8ea8895de83703f7befdf6019ec36ff4ce2
nde-petitos <nde.petitos@gmail.com>
2025-09-17 19:48:34 -0400

    Update Exercise7.tf

--- Exercise7.tf
+++ Exercise7.tf
@@ -1,13 +1,15 @@
 variable "words" {
-   type    = list(string)
-   default = ["hi", "cat", "car", "tree", "hi", "car"]
+   type    = list(string) # Define a variable words as an array of strings
+   default = ["hi", "cat", "car", "tree", "hi", "car"] # Give it a set of default vaalues
 }
 
 locals {
-   lengths_unique = toset([for w in var.words : length(w)])
-   histogram = {
-     for L in local.lengths_unique :
-     tostring(L) => length([for w in var.words : w if length(w) == L])
+   lengths_unique = toset([for w in var.words : length(w)]) # transform the array into a set of unique string length
+   histogram = { 
+      for L in local.lengths_unique : 
+         tostring(L) => length(
+            [for w in var.words : w if length(w) == L]
+         ) 
    }
 }
 
