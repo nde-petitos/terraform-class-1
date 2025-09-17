ce9c7482ec118660bee934f8488674a7e7bfef1f
nde-petitos <nde.petitos@gmail.com>
2025-09-17 19:27:26 -0400

    Create Exercise6.tf

---
+++ Exercise6.tf
@@ -0,0 +1,14 @@
+variable "names" {
+ type    = list(string)
+ default = ["amy", "adam", "bob", "brian", "carol"]
+}
+
+locals {
+ initials = toset([for n in var.names : substr(n, 0, 1)])
+ grouped  = {
+   for i in local.initials :
+   i => [for n in var.names : n if substr(n, 0, 1) == i]
+ }
+}
+
+output "grouped_by_initial" { value = local.grouped }
