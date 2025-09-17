8fd7c069870695d82e2211c804155726eb5dd4a1
nde-petitos <nde.petitos@gmail.com>
2025-09-17 19:20:53 -0400

    Update Exercise4.tf

--- Exercise4.tf
+++ Exercise4.tf
@@ -1,23 +1,17 @@
 variable "items" {
- type    = list(string)
- default = ["b", "a", "c", "a", "b"]
+  type    = list(string)
+  default = ["b", "a", "c", "a", "b"]
 }
-locals {
- unique_sorted = sort(distinct(var.items))
- csv           = join(",", local.unique_sorted)
-}
-output "unique_sorted" { value = local.unique_sorted }
-output "csv"           { value = local.csv }
 
-5) Filter by predicate (keep only even numbers)
-Goal: From a list of numbers, keep evens and also square them.
-variable "nums" {
- type    = list(number)
- default = [1,2,3,4,5,6]
-}
 locals {
- evens   = [for n in var.nums : n if n % 2 == 0]
- squares = [for n in local.evens : n * n]
+  unique_sorted = sort(distinct(var.items))
+  csv           = join(",", local.unique_sorted)
 }
-output "evens"   { value = local.evens }
-output "squares" { value = local.squares }
+
+output "unique_sorted" { 
+  value = local.unique_sorted 
+}
+output "csv"           { 
+  value = local.csv 
+}
+
