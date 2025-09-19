0fac516183bef2ad6ec8c96e2f87f37f80d77a18
nde-petitos <nde.petitos@gmail.com>
2025-09-19 18:30:28 -0400

    Create Exercise9.tf

---
+++ Exercise9.tf
@@ -0,0 +1,15 @@
+variable "hotels" {
+    default = ["Marriot", "Hilton", "Sheraton", "Hyatt"]
+}
+
+variable "activities" {
+    default = ["eat", "sleep", "code", "repeat"]
+}
+
+output "Hotel_upper" {
+    value = [for v in var.var.hotels : v == "Hyatt" ? upper(v) : v]
+} 
+
+output "arrow_display_aactivities" {
+    value = join("->", var.activities)
+}
