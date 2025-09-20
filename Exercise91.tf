cd2fd71cbca11d04245b8718a81fcc054baac354
nde-petitos <nde.petitos@gmail.com>
2025-09-19 20:19:50 -0400

    cygle

--- Exercise91.tf
+++ Exercise91.tf
@@ -8,3 +8,15 @@
 # output "reverse_string" {
 #     value = strrev("Hilton")
 # }
+variable "activities" {
+    default = ["eat", "sleep", "code", "travel", "play"]
+}
+
+variable "foods" {
+    default = ["Pizza", "Burger", "Sushi", "Tacos", "Pasta"]
+}
+
+
+output "cygle" {
+    value = join("", [for v in var.activities : upper(substr(v, 0,1))])
+}
