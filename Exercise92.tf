086e455c8db63596df0b499aa216fd0eb6e4273c
nde-petitos <nde.petitos@gmail.com>
2025-09-24 19:59:48 -0400

    windows web app with US value

--- Exercise92.tf
+++ Exercise92.tf
@@ -7,9 +7,56 @@ variable "canada_items" {
   ]
 }
 
-output "canada_items_transform" {
-  value = {
-    for item in var.canada_items :
+variable "project_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+  default     = "wa"
+}
+
+variable "us_items" {
+  description = "Last-word items for US (Windows web apps)"
+  type        = list(string)
+  default = [
+    "burger", "baseball", "jeans", "hollywood", "donut",
+    "jazz", "applepie", "football", "route66", "hotdog"
+  ]
+}
+
+#############################
+# Windows Web Apps for US items
+#############################
+locals {
+  us_map = {
+    for item in var.us_items :
     item => replace(item, "/[^a-zA-Z0-9-]/", "")
   }
 }
+
+resource "azurerm_windows_web_app" "us_apps" {
+  for_each            = local.us_map
+  name                = "${var.project_prefix}-us-${each.value}-${random_string.suffix.result}"
+  location            = azurerm_service_plan.windows_plan_us.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.windows_plan_us.id
+
+  site_config {
+    always_on = true
+    # Optionally set stack versions here (e.g., dotnet_framework_version) if needed
+  }
+
+  app_settings = {
+    COUNTRY = "us"
+    ITEM    = each.key
+    PREFIX  = "wa-us-${each.key}"
+  }
+
+  tags = {
+    environment = "dev"
+    project     = var.project_prefix
+    country     = "us"
+    item        = each.key
+    os          = "windows"
+  }
+}
+
+
