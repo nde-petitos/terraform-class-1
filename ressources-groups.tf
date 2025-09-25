b14c55484b3e516fe4ede7151bab897d4388e663
nde-petitos <nde.petitos@gmail.com>
2025-09-24 20:27:00 -0400

    remove unecessary ressources

--- ressources-groups.tf
+++ ressources-groups.tf
@@ -1,8 +1,8 @@
-resource "azurerm_resource_group" "terraformmay022025" {
-  name     = "mcit_resource_group_petitos"
-  location = "canadacentral"
-}
-resource "azurerm_resource_group" "terraformseptember" {
-  name     = "mcit_resource_group_petitos"
-  location = "canadacentral"
-}
+# resource "azurerm_resource_group" "terraformmay022025" {
+#   name     = "mcit_resource_group_petitos"
+#   location = "canadacentral"
+# }
+# resource "azurerm_resource_group" "terraformseptember" {
+#   name     = "mcit_resource_group_petitos"
+#   location = "canadacentral"
+# }
